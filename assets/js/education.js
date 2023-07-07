f2b2e291c4c234ebadc9abba45b1ed585e6e4327
kauã dos Santos Silva <kauassilvagithub@gmail.com>
2023-07-07 11:47:35 -0300

    update: Education page to add extra courses and activities section

--- assets/js/education.js
+++ assets/js/education.js
@@ -1,11 +1,13 @@
-const loadmore = document.querySelector('.load-more');
+// Extra education Timeline
+const loadmore1 = document.querySelector('#load-more-1');
 
-let currentItems = 3;
-loadmore.addEventListener('click', (e) => {
-  const elementList = [...document.querySelectorAll('#extra-education .timeline-items .timeline-item')];
+let currentItems1 = 3;
+
+loadmore1.addEventListener('click', (e) => {
+  const elementList = [...document.querySelectorAll('#extra-education-1 .timeline-items .timeline-item')];
   e.target.classList.add('show-loader');
-
-  for (let i = currentItems; i < currentItems + 3; i++) {
+  
+  for (let i = currentItems1; i < currentItems1 + 3; i++) {
     setTimeout( function() {
       e.target.classList.remove('show-loader');
       if (elementList[i]) {
@@ -13,10 +15,37 @@ loadmore.addEventListener('click', (e) => {
       }
     }, 3000);
   }
-  currentItems += 3;
+  currentItems1 += 3;
+  
+  // Hide load button after fully load
+  if (currentItems1 >= elementList.length) {
+    event.target.classList.add('loaded');
+  }
+});
+
+
+
+// Certificates Timeline
+const loadmore2 = document.querySelector('#load-more-2');
+
+let currentItems2 = 3;
+
+loadmore2.addEventListener('click', (e) => {
+  const elementList = [...document.querySelectorAll('#extra-education-2 .timeline-items .timeline-item')];
+  e.target.classList.add('show-loader');
+
+  for (let i = currentItems2; i < currentItems2 + 3; i++) {
+    setTimeout( function() {
+      e.target.classList.remove('show-loader');
+      if (elementList[i]) {
+        elementList[i].style.display = 'flex';
+      }
+    }, 3000);
+  }
+  currentItems2 += 3;
 
   // Hide load button after fully load
-  if (currentItems >= elementList.length) {
+  if (currentItems2 >= elementList.length) {
     event.target.classList.add('loaded');
   }
 });
